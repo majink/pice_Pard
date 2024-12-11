ce27a48aa4fa7f84c8294a205e6659db2558d0c7
하진 <hajin@hajin-ui-MacBookPro.local>
2024-12-11 15:39:17 +0900

    create test_4_long

---
+++ test_4_long/src/main/java/com/example/test_4_long/Test4LongApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_4_long;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test4LongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test4LongApplication.class, args);
+    }
+
+}
---
+++ test_4_long/src/test/java/com/example/test_4_long/Test4LongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_4_long;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test4LongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
